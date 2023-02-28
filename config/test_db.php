ae8bd9b54a5094183be89fa54729a76e4095e800
Alex O'Connor <alexo@southern.edu>
2023-02-27 20:40:27 -0500

    DB Documentation, passing tests

--- config/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db_test;dbname=yii_test',
-    'username' => 'yii_test',
-    'password' => 'yii_test-mysql-password',
-    'charset' => 'utf8',
-];
