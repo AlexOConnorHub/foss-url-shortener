0bd2a66d4aa21577a4ef9cb8a8010a01626b6d54
Alex O'Connor <alexo@southern.edu>
2023-02-27 21:06:11 -0500

    What about this

---
+++ config/test_db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=yii',
+    'username' => 'yii',
+    'password' => 'yii-mysql-password',
+    'charset' => 'utf8',
+];
