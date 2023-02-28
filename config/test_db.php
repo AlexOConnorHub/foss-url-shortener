6c26a31240ff02d8b727fee8b5bbf25a4d137e35
Alex O'Connor <alexo@southern.edu>
2023-02-27 21:38:32 -0500

    Add DB

--- config/test_db.php
+++ config/test_db.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=yii',
-    'username' => 'yii',
-    'password' => 'yii-mysql-password',
+    'dsn' => 'mysql:host=db_test;dbname=yii_test',
+    'username' => 'yii_test',
+    'password' => 'yii_test-mysql-password',
     'charset' => 'utf8',
 ];
