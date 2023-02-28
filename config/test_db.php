913740d511cf089deafd1fc01bae5e4d343fdac3
Alex O'Connor <alexo@southern.edu>
2023-02-27 21:57:02 -0500

    DB before split

--- config/test_db.php
+++ config/test_db.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db_test;dbname=yii_test',
+    'dsn' => 'mysql:host=localhost;dbname=yii_test',
     'username' => 'yii_test',
     'password' => 'yii_test-mysql-password',
     'charset' => 'utf8',
